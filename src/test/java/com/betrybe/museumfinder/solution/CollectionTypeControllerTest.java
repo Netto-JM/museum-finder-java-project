cd9f2464acbf538b2083a3de1fc01f3c5025c531
NettoJM <nettojm86@gmail.com>
2023-09-12 00:13:50 -0300

    Remove Mokito usage from tests for better coverage

--- src/test/java/com/betrybe/museumfinder/solution/CollectionTypeControllerTest.java
+++ src/test/java/com/betrybe/museumfinder/solution/CollectionTypeControllerTest.java
@@ -1,22 +1,24 @@
 package com.betrybe.museumfinder.solution;
 
-import com.betrybe.museumfinder.dto.CollectionTypeCount;
+// import com.betrybe.museumfinder.dto.CollectionTypeCount;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.mockito.Mockito;
+// import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import com.betrybe.museumfinder.service.CollectionTypeService;
+// import com.betrybe.museumfinder.service.CollectionTypeService;
 import org.springframework.http.MediaType;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@SpringBootTest
+// Previous tests, for reference:
+
+/*@SpringBootTest
 @AutoConfigureMockMvc
 @DisplayName("Tests for CollectionTypeController")
 public class CollectionTypeControllerTest {
@@ -50,5 +52,32 @@ public class CollectionTypeControllerTest {
 
     mockMvc.perform(get("/collections/count/{typesList}", "no,match"))
       .andExpect(status().isNotFound());
+  }
+}*/
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Tests for CollectionTypeController")
+public class CollectionTypeControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @DisplayName("Should return the number of Museums with certain collection types.")
+  public void testMuseumCollectionTypesCount() throws Exception {
+    String[] collectionTypes = {"hist","imag"};
+
+    mockMvc.perform(get("/collections/count/{typesList}", "hist,imag"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.count").value(492));
+  }
+
+  @Test
+  @DisplayName("Should return 404 when no Museums are found with certain collection types.")
+  public void testMuseumCollectionTypesNotFound() throws Exception {
+
+    mockMvc.perform(get("/collections/count/{typesList}", "noMatchHere"))
+        .andExpect(status().isNotFound());
   }
 }
